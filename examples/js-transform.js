0b70964b659427340e4de0ee7cd6b55351b94550
Wangchong Zhou <fffonion@gmail.com>
2021-04-26 21:12:55 +0800

    doc: add an example transformer plugin (#6)

---
+++ examples/js-transform.js
@@ -0,0 +1,55 @@
+'use strict';
+
+let proc = require("process")
+
+// This is an example plugin that appends a string in response body
+
+class KongPlugin {
+  constructor(config) {
+    this.config = config
+  }
+
+  async access(kong) {
+    // Only need the following if response handler not exist
+    // buffered proxying is automatically turned on for plugin
+    // with a response handler
+    // await kong.service.request.enableBuffering()
+
+    let requestBody = await kong.request.getRawBody()
+    // convert it to uppercase
+    requestBody = requestBody.replace(/(.)/g, function(v) { return v.toUpperCase(); })
+
+    await Promise.all([
+      // append "?js-transform=v0.1.0" to request line to upstream
+      kong.service.request.setQuery({
+        "js-transform": "v0.1.0",
+      }),
+      // set the transformed request body to upstream
+      kong.service.request.setRawBody(requestBody),
+    ])
+  }
+
+  // Note: by defining reponse handler implictly turns on buffered proxying
+  // on the Route/Service and may break connections like WebSocket
+  // and has performance penalty
+  async response(kong) {
+    if (await kong.response.getSource() == "service") {
+      let body = await kong.service.response.getRawBody()
+
+      body = "Response body from upstream:\n" + body + "\nBody size: " + body.length + "\n"
+
+      await kong.response.exit(await kong.response.getStatus(), body)
+    }
+  }
+}
+
+module.exports = {
+  Plugin: KongPlugin,
+  Schema: [{
+    message: {
+      type: "string"
+    }
+  }, ],
+  Version: '0.1.0',
+  Priority: 0,
+}
