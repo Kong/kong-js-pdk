167e56c2b1de07efd345bfddafff8ae7201e7a9b
Wangchong Zhou <fffonion@gmail.com>
2021-05-19 16:01:41 +0800

    fix: reorganize dependencies (#8)

--- examples/js-transform.test.js
+++ examples/js-transform.test.js
@@ -1,40 +1,41 @@
 const plugin = require('./js-transform');
 
 const {
-	PluginTest,
-	Request
-} = require("../plugin_test")
+  PluginTest,
+  Request
+} = require("kong-pdk/plugin_test")
 
 
 test('Set headers in response', async () => {
-	let r = new Request()
+  let r = new Request()
 
-	r.useURL("http://example.com")
-		.useMethod("GET")
-		.useHeaders({
-			"Host": "example.com",
-		})
-		.useBody("all lower case")
+  r
+    .useURL("http://example.com")
+    .useMethod("GET")
+    .useHeaders({
+      "Host": "example.com",
+    })
+    .useBody("all lower case")
 
-	let t = new PluginTest(r)
+  let t = new PluginTest(r)
 
-	await t.Run(plugin, {})
+  await t.Run(plugin, {})
 
-	expect(t.serviceRequest.body)
-		.toBe('ALL LOWER CASE')
+  expect(t.serviceRequest.body)
+    .toBe('ALL LOWER CASE')
 
-	expect(t.serviceRequest.url.search)
-		.toBe('?js-transform=v0.1.0')
+  expect(t.serviceRequest.url.search)
+    .toBe('?js-transform=v0.1.0')
 
-	expect(t.response.status)
-		.toBe(200)
+  expect(t.response.status)
+    .toBe(200)
 
-	expect(t.serviceResponse.body)
-		.toBe("OK")
+  expect(t.serviceResponse.body)
+    .toBe("OK")
 
-	expect(t.response.body)
-		.toBe(
-			`Response body from upstream:
+  expect(t.response.body)
+    .toBe(
+      `Response body from upstream:
 OK
 Body size: 2
 `)
